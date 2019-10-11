d523bae66381213c24dc3464dd136e1cbbbdf522
anderoma <antoine.deroma@gmail.com>
2019-10-11 08:11:22 +0200

    modif_mairie

--- mairie_christmas/lib/app.rb
+++ mairie_christmas/lib/app.rb
@@ -4,20 +4,20 @@ require 'nokogiri'
 require 'rubygems'
 
 
-# get_townhall_valdoise_url vas chercher TOUS les urls de toutes les mairies du val d'oise
-def get_townhall_valdoise_url
-    townhall_link = "http://annuaire-des-mairies.com/val-d-oise.html"
-    townhallurls = Array.new
-    page = Nokogiri::HTML(open(townhall_link))
-    geturl = page.css('//[@class="lientxt"]').each do |link|
+# get_city_valdoise_url vas chercher TOUS les urls de toutes les mairies du val d'oise
+def get_city_valdoise_url
+    valdoise_url = "http://annuaire-des-mairies.com/val-d-oise.html"
+    city_urls = Array.new
+    page = Nokogiri::HTML(open(valdoise_url))
+    get_url = page.css('//[@class="lientxt"]').each do |link|
         url = link['href']
-        townhallurls << url
+        city_urls << url
     end
-    return townhallurls
+    return city_urls
 end
 
 # get_townhall_email recupere les emails de chaques site de chaques mairie via les urls récupérés plus haut
-def get_townhall_email(url)
+def get_city_email(url)
     url1 = "https://www.annuaire-des-mairies.com/"
         emails = Array.new
         page = Nokogiri::HTML(open(url1 + url)) # ici on ajoute a l'url source l'extension correspondant a chacunes des villes
@@ -28,10 +28,10 @@ def get_townhall_email(url)
 end
 
 # finalement get_townhall_names va trouver et mettre dans un tableau tous les nom des villes de tout le val d'oise
-def get_townhall_names
-    townhall_link = "http://annuaire-des-mairies.com/val-d-oise.html"
+def get_city_names
+    valdoise_url = "http://annuaire-des-mairies.com/val-d-oise.html"
     names = Array.new
-    name = Nokogiri::HTML(open(townhall_link))
+    name = Nokogiri::HTML(open(valdoise_url))
     name.xpath('//a[contains(@href, "95")]').each do |n|
         names.push(n.text)
     end
@@ -40,18 +40,18 @@ end
 
 # perform execute et affiche l'ensemble du programme
 def perform()
-    hyp = get_townhall_valdoise_url
-    city = get_townhall_names
+    hyp = get_city_valdoise_url
+    city_name = get_city_names
+    display = 0
     i = 0
-    j = 0
-    while i < get_townhall_valdoise_url.length
+    while display < get_city_valdoise_url.length
         puts "==============================================================="
-        print city[j]
+        print city_name[display]
         print "  =>  "
-        puts get_townhall_email(hyp[j])
+        puts get_city_email(hyp[display])
         puts "==============================================================="
         i = i + 1
-        j = j + 1
+        display = display + 1
     end
 end
 
